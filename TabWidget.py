015b4942dfd2508bd4caa206db8117f5b5b0be9f
Anand Srivastava <linkinAnand@gmail.com>
2011-04-17 20:45:13 -0700

    Resolving syntax error update on line-148 , indentation error solved.

--- TabWidget.py
+++ TabWidget.py
@@ -145,9 +145,9 @@ class TabWidget(QtGui.QTableWidget) :
 		for i in range(len(obj.partition)) :
 			dL+=int(obj.partition[i][2])
 			
-                if(obj.length == 0)
+                if int(obj.length) == 0 :
 			self.setCellWidget(row,2,self.getLabel(str(0)+'%',str(0)))
-		else
+		else :
 			self.setCellWidget(row,2,self.getLabel(str((100*dL)/int(obj.length))+'%',str(100*dL/int(obj.length))))
 		self.setCellWidget(row,3,self.getLabel(' - ',status))
 		self.setCellWidget(row,4,self.getLabel(obj.date,str(obj.date)))				

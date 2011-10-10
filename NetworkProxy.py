d980b05325df6a397d02a5a1e3f3446811ea72b4
AnandSrivastava <linkinAnand@gmail.com>
2011-10-10 18:45:12 +0530

    EncodingFileAdded

---
+++ NetworkProxy.py
@@ -0,0 +1,129 @@
+import sys,os,datetime,time
+from PyQt4 import QtGui,QtCore,QtNetwork
+from PyQt4.QtWebKit import *
+from PyQt4.QtNetwork import *
+from UIXML import UIXML
+
+class NetworkProxy(QtGui.QWidget) :
+	def __init__(self,parent=None,loc=None,user=None) :
+		QtGui.QWidget.__init__(self,parent)	
+		self.setWindowTitle('Network Configuration')	
+		self.username = user
+		self.authenticated = 1
+		self.posMove=None	
+		
+		self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+								
+		self.uixml = UIXML(os.getcwd())
+		okay = QtGui.QPushButton('Ok')
+		cancel = QtGui.QPushButton('Cancel')
+		
+		hbox3 = QtGui.QHBoxLayout()		hbox3.addWidget(okay)
+		hbox3.addWidget(cancel)
+		
+		lhttp = QtGui.QLabel('HTTPProxy: ')
+		lport = QtGui.QLabel('Port: ')
+		self.http = QtGui.QLineEdit()
+		self.port = QtGui.QLineEdit()
+		
+		hbox1 = QtGui.QHBoxLayout()
+		hbox1.addWidget(lhttp)
+		hbox1.addWidget(self.http)
+		hbox1.addWidget(lport)
+		hbox1.addWidget(self.port)
+		
+		self.cbox = QtGui.QCheckBox('No Authentication')
+		
+		userl = QtGui.QLabel('Username: ')
+		passwrdl = QtGui.QLabel('Password: ')
+		self.user = QtGui.QLineEdit()
+		self.passwrd = QtGui.QLineEdit()
+		self.passwrd.setEchoMode(2)
+		
+		hbox2 = QtGui.QHBoxLayout()
+		hbox2.addWidget(userl)
+		hbox2.addWidget(self.user)
+		hbox2.addWidget(passwrdl)
+		hbox2.addWidget(self.passwrd)
+		
+		vbox = QtGui.QVBoxLayout()
+		vbox.addLayout(hbox1)
+		vbox.addWidget(self.cbox)
+		vbox.addLayout(hbox2)
+		vbox.addLayout(hbox3)
+		
+		self.setLayout(vbox)				
+		self.setUsername(self.username)
+			
+		self.setWindowTitle('Network Proxy')
+		self.connect(self.cbox,QtCore.SIGNAL('stateChanged(int)'),self.Authenticate)
+		self.connect(okay,QtCore.SIGNAL('clicked()'),self.Finalize)
+		self.connect(cancel,QtCore.SIGNAL('clicked()'),self,QtCore.SLOT('close()'))
+	
+	def mousePressEvent(self,event) :
+		self.posMove = event.pos()
+	
+	def mouseMoveEvent(self,event) :
+		if self.posMove :
+			self.move(self.pos().x()+event.pos().x()-self.posMove.x(),self.pos().y()+event.pos().y()-self.posMove.y())
+	
+	def mouseReleaseEvent(self,event) :
+		self.posMove=None
+			
+	def getInformation(self) :
+		l=[]
+		if not(len(str(self.http.text()))) or not(len(str(self.port.text()))) :			
+			return []
+		l.append(str(self.http.text()))
+		l.append(str(self.port.text()))
+		if len(str(self.user.text())) and len(str(self.passwrd.text())) :				
+			l.append(str(self.user.text()))
+			l.append(str(self.passwrd.text()))
+		
+		return l
+		
+	def setUsername(self,user) :
+		self.username = user
+		l=self.uixml.getProxyInformation(self.username)
+		if len(l)==2:
+			self.http.setText(str(l[0]))
+			self.port.setText(str(l[1]))
+		elif len(l) == 4:
+			self.user.setText(str(l[2]))
+			self.passwrd.setText(str(l[3]))
+		else :
+			self.cbox.setChecked(1)
+			self.authenticated=0
+			self.user.setReadOnly(1)
+			self.passwrd.setReadOnly(1)
+			
+	def Authenticate(self,state) :		
+		if state == 2:
+			self.authenticated = 0
+			self.user.setReadOnly(True)
+			self.passwrd.setReadOnly(True)
+		else :
+			self.authenticated = 1
+			self.user.setReadOnly(False)
+			self.passwrd.setReadOnly(False)
+		
+	def Finalize(self) :		
+		l=[]
+		if not(len(str(self.http.text()))) or not(len(str(self.port.text()))) :
+			QtGui.QMessageBox.information(self,'Warning','http/port field is empty')
+			return				
+		
+		l.append(str(self.http.text()))
+		l.append(str(self.port.text()))
+				
+		if self.authenticated :
+			if not(len(str(self.user.text()))) or not(len(str(self.passwrd.text()))) :
+				QtGui.QMessageBox.information(self,'Warning','login/passwrd field is empty')
+				return
+			else :
+				l.append(str(self.user.text()))
+				l.append(str(self.passwrd.text()))	
+			
+		if self.uixml.setProxyInformation(self.username,l):
+			QtGui.QMessageBox.information(self,'Okay','Done Successfully')
+		self.close()
---
+++ UIXML.py
@@ -0,0 +1,163 @@
+import os,sys,hashlib
+from xml.dom import minidom
+from xml.dom.minidom import Document
+from Crypto.Cipher import AES
+
+class UIXML :
+	def __init__(self,loc = None) :		
+		self.loc = loc
+		self.aes = AES.new('CRLKUTSPALMWYTDB',AES.MODE_ECB)
+	
+	def bringTo32(self,text) :
+		if len(text) >= 32 :
+			return text[0:32]
+		else :
+			for i in range(32-len(text)) :
+				text = text+' '
+		return text
+	
+	def crypt(self,passwrd) :		
+		ciphercode=self.aes.encrypt(self.bringTo32(passwrd))
+		return ciphercode
+		
+	def decrypt(self,passwrd) :
+		data = self.aes.decrypt(passwrd)
+		return data
+		
+	def generateXML(self) :
+		doc = Document()
+		ui = doc.createElement('userinfo')
+		doc.appendChild(ui)
+		return doc
+		
+	def createChild(self,doc,text) :
+		ptext = doc.createTextNode(text)
+		return ptext
+		
+	def getProxyInformation(self,user) :
+		l=[]	
+		try :
+			xmldoc = minidom.parse(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest())+'/Uinfo')	
+
+			doc = xmldoc.firstChild
+			l.append(doc.getElementsByTagName('http')[0].firstChild.data)
+			l.append(doc.getElementsByTagName('port')[0].firstChild.data)		
+			try :
+				passwrd = file(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest())+'/lzxxslfd','rb')
+				l.append(doc.getElementsByTagName('login')[0].firstChild.data)
+				password = passwrd.read()			
+				l.append(self.decrypt(password))
+				passwrd.close()
+			except :
+				pass
+		except :
+			return []
+		return l
+		
+	def setProxyInformation(self,user,info) :
+		try :
+			if not os.path.isdir(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest())) :	
+				os.mkdir(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest()))
+			xmldoc = minidom.parse(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest()+'/Uinfo'))
+		except :
+			xmldoc = self.generateXML()
+			
+		doc = xmldoc.firstChild
+					
+		if len(info) >= 2 :
+			try :
+				doc.removeChild(doc.getElementsByTagName('http')[0])
+			except :				pass
+			child = xmldoc.createElement('http')
+			child.appendChild(self.createChild(xmldoc,info[0]))
+			doc.appendChild(child)
+			try :
+				doc.removeChild(doc.getElementsByTagName('port')[0])
+			except :
+				pass
+			child = xmldoc.createElement('port')
+			child.appendChild(self.createChild(xmldoc,info[1]))
+			doc.appendChild(child)
+			try :			
+				doc.removeChild(doc.getElementsByTagName('login')[0])
+			except :
+				pass
+		if len(info) == 4 :
+			try :
+				passwrd = open(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest())+'/lzxxslfd','wb')
+				passwrd.write(self.crypt(info[3]))
+				passwrd.close()
+				child = xmldoc.createElement('login')
+				child.appendChild(self.createChild(xmldoc,info[2]))
+				doc.appendChild(child)
+			except :
+				pass
+			
+		f = open(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest())+'/Uinfo', 'w')
+		xmldoc.writexml(f)
+		f.close()
+		return 1
+		
+	def getSchedule(self,user) :
+		try :
+			xmldoc = minidom.parse(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest())+'/Uinfo')
+		except :
+			return []
+			
+		l=[]	
+		dict = {}
+		schedule = xmldoc.firstChild.getElementsByTagName('schedule')
+		if len(schedule) :
+			schedule = schedule[0]
+		else :
+			return []
+		sched = schedule.getElementsByTagName('sched')
+		if len(sched) >= 7:
+			for i in range(7) :
+				l.append(sched[i].firstChild.data)
+			for i in range(7,len(sched)) :	
+				try :
+					dict[sched[i].attributes['date'].value]=sched[i].firstChild.data
+				except :
+					dict[sched[i].attributes['date'].value]='  '
+				
+		l.append(dict)		
+		return l
+		
+	def setSchedule(self,user,info) :
+		try :
+			if not os.path.isdir(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest())) :
+				os.mkdir(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest()))
+			xmldoc = minidom.parse(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest())+'/Uinfo')
+		except :
+			xmldoc = self.generateXML()
+			
+		if len(info) < 7:
+			return 0
+			
+		doc = xmldoc.firstChild.getElementsByTagName('schedule')
+		if len(doc) :
+			doc = doc[0]
+		else :
+			doc = xmldoc.createElement('schedule')
+			xmldoc.firstChild.appendChild(doc)
+			
+		days = doc.getElementsByTagName('sched')
+		for node in days :
+			doc.removeChild(node)			
+		for i in range(7) :
+			child = xmldoc.createElement('sched')
+			child.appendChild(self.createChild(xmldoc,info[i]))			
+			doc.appendChild(child)			
+		dict = info[7]
+		for key in dict :
+			child = xmldoc.createElement('sched')
+			child.appendChild(self.createChild(xmldoc,dict[key]))
+			child.setAttribute('date',key)
+			doc.appendChild(child)
+		
+		file = open(os.getcwd()+'/'+str(hashlib.sha224(user).hexdigest())+'/Uinfo','w')
+		xmldoc.writexml(file)
+		file.close()
+		
+		return 1
